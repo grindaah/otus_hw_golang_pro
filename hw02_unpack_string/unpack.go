49e91dd574993cfdc00c43762c14fd338bf89457
Victor Lifagin <grindaah@google.com>
2022-08-19 21:13:20 +0400

    draft

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,67 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	var result strings.Builder
+	var cursorAlhpa rune
+	var escapedNext bool
+	unpack := func(sym rune, i int) {
+		result.WriteString(strings.Repeat(string(cursorAlhpa), i))
+		cursorAlhpa = 0
+		escapedNext = false
+	}
+	for _, r := range s {
+		if !unicode.IsDigit(r) {
+			// check for backslash
+			if r == 0x5C {
+				if !escapedNext {
+					escapedNext = true
+				} else {
+					if cursorAlhpa != 0 {
+						unpack(cursorAlhpa, 1)
+					}
+					cursorAlhpa = r
+				}
+
+				continue
+			} else {
+				if cursorAlhpa != 0 {
+					unpack(cursorAlhpa, 1)
+				}
+				cursorAlhpa = r
+			}
+		} else {
+			//is digit
+			if escapedNext {
+				cursorAlhpa = r
+				continue
+				//
+			} else {
+				if cursorAlhpa == 0 {
+					return "", ErrInvalidString
+				}
+				digit, err := strconv.Atoi(string(r))
+				if err != nil {
+					return "", err
+				}
+				unpack(cursorAlhpa, digit)
+			}
+			if escapedNext {
+				cursorAlhpa = r
+			}
+		}
+	}
+	// check last symbol
+	if cursorAlhpa != 0 {
+		unpack(cursorAlhpa, 1)
+	}
+
+	return result.String(), nil
 }
