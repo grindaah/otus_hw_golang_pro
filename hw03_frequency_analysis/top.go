6e603a8a2d4ee07726763f47f4d70414573948b6
Victor Lifagin <grindaah@google.com>
2022-08-22 11:55:54 +0400

    draft

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,59 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+type WordCounter struct {
+	Word  string
+	Count int
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func Top10(s string) []string {
+	if len(s) == 0 {
+		return []string{}
+	}
+	wordsMap := make(map[string]int, 0)
+	wordsSlice := make([]WordCounter, 0)
+
+	flds := strings.Fields(s)
+	for _, fld := range flds {
+		normalizedString := normalizeString(fld)
+		if fld != "" {
+			wordsMap[normalizedString]++
+		}
+	}
+	for k, v := range wordsMap {
+		wordsSlice = append(wordsSlice, WordCounter{Word: k, Count: v})
+	}
+
+	sort.Slice(wordsSlice, func(i, j int) bool {
+		return wordsSlice[i].Count > wordsSlice[j].Count
+	})
+	sort.Slice(wordsSlice, func(i, j int) bool {
+		return wordsSlice[i].Count == wordsSlice[j].Count && wordsSlice[i].Word < wordsSlice[j].Word
+	})
+
+	sz := min(10, len(wordsSlice))
+	result := make([]string, sz)
+	for i := 0; i < sz; i++ {
+		result[i] = wordsSlice[i].Word
+	}
+
+	return result
+}
+
+func normalizeString(s string) string {
+	r := regexp.MustCompile("\\b.*,?\\b")
+	w := r.FindString(s)
+	return w
 }
