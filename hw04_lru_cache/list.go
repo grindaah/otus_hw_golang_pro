2a06adaa8a7d988665a59250b675cf0df9c5f031
Victor Lifagin <grindaah@google.com>
2022-08-25 12:23:46 +0400

    draft for double-linked list

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,8 +17,99 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
+	//List // Remove me after realization.
 	// Place your code here.
+	front  *ListItem
+	back   *ListItem
+	length int
+	//store map[*ListItem]interface{}
+}
+
+func (l *list) Len() int {
+	return l.length
+}
+
+func (l *list) Front() *ListItem {
+	return l.front
+}
+
+func (l *list) Back() *ListItem {
+	return l.back
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	if l.front != nil {
+		li := &ListItem{
+			Next:  l.front,
+			Prev:  nil,
+			Value: v,
+		}
+		l.front.Prev = li
+		l.front = li
+		l.length++
+		return li
+	} else {
+		return l.insertFirstItem(v)
+	}
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	if l.back != nil {
+		li := &ListItem{
+			Next:  nil,
+			Prev:  l.back,
+			Value: v,
+		}
+		l.back.Next = li
+		l.back = li
+		l.length++
+		return li
+	} else {
+		return l.insertFirstItem(v)
+	}
+}
+
+func (l *list) insertFirstItem(v interface{}) *ListItem {
+	li := &ListItem{
+		Next:  nil,
+		Prev:  nil,
+		Value: v,
+	}
+	l.back = li
+	l.front = li
+	l.length++
+	//l.store[li] = nil
+	return li
+}
+
+func (l *list) Remove(i *ListItem) {
+	if i == nil {
+		return
+	}
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	}
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	}
+	if i == l.front {
+		l.front = i.Next
+	}
+	if i == l.back {
+		l.back = i.Prev
+	}
+
+	l.length--
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	if i == l.front {
+		return
+	}
+	i.Prev.Next = i.Next
+	i.Next = l.front
+	l.front.Prev = i
+	i.Prev = nil
 }
 
 func NewList() List {
