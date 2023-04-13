7196a5a1c7a5517dd0475cbae44a44ddabece0e9
Victor Lifagin <grindaah@google.com>
2023-04-13 09:48:24 +0100

    fixed incomplete writing

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
 )
+
+const chunkSizeDefault = 4096
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
@@ -10,6 +14,63 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	f, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	var sz int64
+	sz, err = validateFile(f)
+	if err != nil {
+		return err
+	}
+
+	toFile, errCreate := os.Create(toPath)
+	if errCreate != nil {
+		return errCreate
+	}
+	toFile.Chmod(0644 | os.ModeAppend)
+	defer toFile.Close()
+
+	//f.Seek(offset, 0)
+	currentOffset := offset
+	chunkSize := int64(chunkSizeDefault)
+	var written int64
+
+	for {
+		if sz-currentOffset < chunkSizeDefault {
+			chunkSize = sz - currentOffset
+		}
+
+		f.Seek(currentOffset, 0)
+		toFile.Seek(currentOffset, 0)
+		wr, errW := io.CopyN(toFile, f, chunkSize)
+		if errW != nil {
+			return errW
+		}
+		written = written + wr
+		if written >= sz-currentOffset {
+			break
+		}
+		currentOffset += chunkSize
+	}
+
 	return nil
+}
+
+func validateFile(f *os.File) (int64, error) {
+	stat, err := f.Stat()
+	if err != nil {
+		return 0, err
+	}
+	if stat.IsDir() {
+		// TODO may be separate error for this
+		return 0, ErrUnsupportedFile
+	}
+	sz := stat.Size()
+
+	if offset > sz {
+		return sz, ErrOffsetExceedsFileSize
+	}
+	return sz, nil
 }
